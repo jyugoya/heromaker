ef59013a726f6c2d4570106ba23d9edc3d8875c5
Yura Yuki <jyugoya@gmail.com>
2012-02-12 21:43:52 +0900

    Just initialized

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heromaker::Application.config.secret_token = '4e1510315eb9d78a153536ebcb62ed87a4e0cc2a8610dba2c72532a6a019102f4d59faead09cf341de9cb872bd1fcfabdbf4f397d60ecc18953a0cdfa68b38fb'
